132a34754d90f20a58ee6a34ea4b306101ea4490
Jamal Frank <jamaljfrank@gmail.com>
2019-09-17 05:12:35 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,11 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
-  attr_reader :title
+  
  
   def initialize(brand)
     @brand = brand
   end
- 
+ def cobble
+   
   
 end
